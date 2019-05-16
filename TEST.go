283e0f0d547e31fc393c2371494432b2aff5386a
THEragamuffin <50669748+THEragamuffin@users.noreply.github.com>
2019-05-16 11:36:51 -0400

    Update TEST.go

--- TEST.go
+++ TEST.go
@@ -3,7 +3,9 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println("Rory is a stinky doo doo head.")
+    for i := 1; i <= 1000; i++ {
+       fmt.Println("Rory is a stinky doo doo head.")
+    }
 fmt.Println("")
 fmt.Println("")
 fmt.Println("oh and hello world")
