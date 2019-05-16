157631f828b93498c1e7afdbddbf8b3aa153701f
THEragamuffin <50669748+THEragamuffin@users.noreply.github.com>
2019-05-16 12:00:36 -0400

    Update TEST.go

--- TEST.go
+++ TEST.go
@@ -1,8 +1,9 @@
 package main
 
 import "fmt"
-fmt.Println("Version 1.10")
 func main(){
+    fmt.Println("Version 1.10")
+
     for i := 1; i <= 1000; i++ {
        fmt.Println("Rory is a stinky doo doo head.")
     }
